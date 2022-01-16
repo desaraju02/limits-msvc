9502590b658e3ab5eb91a743a0728d59176fafe4
desaraju02 <desaraju02@gmail.com>
2022-01-16 14:46:27 +0530

    Basic Spring Boot Package

---
+++ src/main/java/com/nag/limitsmicrosvcsv2/LimitsMicroSvcsV2Application.java
@@ -0,0 +1,13 @@
+package com.nag.limitsmicrosvcsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsMicroSvcsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsMicroSvcsV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nag/limitsmicrosvcsv2/LimitsMicroSvcsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nag.limitsmicrosvcsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsMicroSvcsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
